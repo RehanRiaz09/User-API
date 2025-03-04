54e421c7a7ea77e5efefa9e48097d4fffb9327f7
Syed Rehan Riaz <collectioninvestor@gmail.com>
2025-03-04 12:53:51 +0500

    new

--- config/database.js
+++ config/database.js
@@ -1,7 +1,8 @@
 import mongoose from 'mongoose';
+const dotenv = await import('dotenv');
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb://127.0.0.1:27017/testdb');
+    await mongoose.connect(process.env.MONGO_URL);
     console.log(`MongoDB is connected`);
   } catch (error) {
     console.error(error.message);
